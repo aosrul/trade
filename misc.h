45b84c5443961fad5a904facecbeb7724f5f35fe
vincent.ayala@gmail.com <pi@raspberrypi>
2014-10-12 18:14:29 +0200

    updated header

--- misc.h
+++ misc.h
@@ -11,5 +11,4 @@ int		my_write(char *str, int size);
 void		*xmalloc(int size);
 int		x_get_nbr(void);
 void		xfree(void *buff);
-int		my_print_infos(t_struct *s, int count, int option, FILE *file);
 #endif /* _MISC_H_ */
